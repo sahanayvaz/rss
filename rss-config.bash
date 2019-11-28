3e229284ce93ec52bc9b37c61e309a04b9807a83
sahanayvaz <sahanayvaz@gmail.com>
2019-11-28 07:49:52 +0100

    added random noise experiment

--- rss-config.bash
+++ rss-config.bash
@@ -305,3 +305,32 @@ do
                               --freeze_weights=$freeze_weights \
                               --exp_name=$exp_name --evaluation=0
 done
+
+
+
+for policy_spec in "ls_c_v0"
+do
+     exp="RSS"
+     
+     exp_name="MARIO-1-1-$exp-NOISE-v0"
+
+     python3 create_config.py --env_kind="mario" --env_id="SuperMarioBros-1-1-v0" \
+                              --NUM_ENVS=8 --NUM_LEVELS=0 \
+                              --SET_SEED=0 --PAINT_VEL_INFO=0 --USE_DATA_AUGMENTATION=0 --GAME_TYPE="standard" \
+                              --USE_BLACK_WHITE=0 --IS_HIGH_RES=0 --HIGH_DIFFICULTY=0 --nframeskip=4 \
+                              --use_news=1 --lambda=0.95 --gamma=0.99 --nepochs=3 --nminibatches=8 \
+                              --lr_lambda=0 --lr=1e-4 --cliprange_lambda=0 --cliprange=0.1 \
+                              --norm_obs=1 --norm_adv=1 --norm_rew=1 --clip_rew=1 --ent_coeff=0.001 \
+                              --vf_coef=1.0 --max_grad_norm=40.0 --num_timesteps=64000000 \
+                              --early_final=6000000 \
+                              --jacobian_loss=0 --beta_jacobian_loss=0 --tol_jacobian_loss=0 \
+                              --entity_loss=0 --beta_entity_loss=0 \
+                              --nsteps=128 \
+                              --input_shape="84x84" --perception="nature_cnn" --feat_spec="feat_rss_v0" \
+                              --policy_spec=$policy_spec --activation="relu" \
+                              --layernormalize=0 --batchnormalize=0 \
+                              --attention=0 --recurrent=0 \
+                              --add_noise=1 --keep_noise=30 --noise_std=0.1 \
+                              --save_interval=$save_interval --save_dir=$save_dir --log_dir="./log_dir" \
+                              --exp_name=$exp_name --evaluation=0
+done
