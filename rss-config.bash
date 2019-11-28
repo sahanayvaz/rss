e102c32f29f8a3853e556eb30749ed7b44997df1
sahanayvaz <sahanayvaz@gmail.com>
2019-11-28 05:28:39 +0100

    added world 2-2 baseline experiment

--- rss-config.bash
+++ rss-config.bash
@@ -185,3 +185,24 @@ do
                               --exp_name=$exp_name --evaluation=0
 done
 
+exp_name=MARIO-2-2-baseline-v0
+python3 create_config.py --env_kind="mario" --env_id="SuperMarioBros-2-2-v0" \
+                         --NUM_ENVS=8 --NUM_LEVELS=0 \
+                         --SET_SEED=0 --PAINT_VEL_INFO=0 --USE_DATA_AUGMENTATION=0 --GAME_TYPE="standard" \
+                         --USE_BLACK_WHITE=0 --IS_HIGH_RES=0 --HIGH_DIFFICULTY=0 --nframeskip=4 \
+                         --use_news=1 --lambda=0.95 --gamma=0.99 --nepochs=3 --nminibatches=8 \
+                         --lr_lambda=0 --lr=1e-4 --cliprange_lambda=0 --cliprange=0.1 \
+                         --norm_obs=1 --norm_adv=1 --norm_rew=1 --clip_rew=1 --ent_coeff=0.001 \
+                         --vf_coef=1.0 --max_grad_norm=40.0 --num_timesteps=64000000 \
+                         --early_final=12000000 \
+                         --jacobian_loss=0 --beta_jacobian_loss=0 --tol_jacobian_loss=0 \
+                         --entity_loss=0 --beta_entity_loss=0 \
+                         --nsteps=128 \
+                         --input_shape="84x84" --perception="nature_cnn" --feat_spec="feat_v0" \
+                         --policy_spec="ls_c_v0" --activation="relu" \
+                         --layernormalize=0 --batchnormalize=0 \
+                         --attention=0 --recurrent=0 \
+                         --add_noise=0 \
+                         --for_visuals=1 \
+                         --save_interval=$save_interval --save_dir=$save_dir --log_dir="./log_dir" \
+                         --exp_name=$exp_name --evaluation=0
