bc6db3fdbfd3e6f60cbc81659ae3f0eb0b80665c
sahanayvaz <sahanayvaz@gmail.com>
2019-12-07 02:03:05 +0100

    added automatic experiment restarter

--- check_exp.py
+++ check_exp.py
@@ -14,9 +14,8 @@ for d in dirs:
         if iters[-1] != 1464 and not('NSTD-1.0' in d or 'NSTD-0.5' in d):
             print('restarting experiments:', d, iters[-1])
             model_spec = os.path.join(HOME, 'model_specs', '{}.json'.format(d))
-            call(["bsub", "-n", "8", "python3", "run.py", 
-                  "--server_type", "LEONHARD", 
-                  "--model_spec", model_spec, 
-                  "--restore_iter", iters[-1]])
+            subcommand = "python3 run.py --server_type LEONHARD --model_spec {} --restore_iter {}".format(model_spec, iters[-1])
+            command = "bsub -n 8 '{}'".format(subcommand)
+            os.system(command)
     except:
         pass
