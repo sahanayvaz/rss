b9d9d51e86abae2fcfab0496140a692b84d241b8
sahanayvaz <sahanayvaz@gmail.com>
2019-12-07 04:25:36 +0100

    separated log_dir for transfer tasks

--- check_exp.py
+++ check_exp.py
@@ -8,21 +8,18 @@ dir_path = os.path.join(HOME, 'log_dir')
 dirs = os.listdir(dir_path)
 
 for d in dirs:
-    try:
-        csv = os.path.join(dir_path, d, 'inter', 'progress.csv')
-        data = pd.read_csv(csv)
-        iters = data['iter'].tolist()
-        if 'TRD' in d:
-            dir_path = os.path.join(HOME, 'log_dir', d)
-            print(dir_path)
-            shutil.rmtree(dir_path)
-            print('removed')
+    csv = os.path.join(dir_path, d, 'inter', 'progress.csv')
+    data = pd.read_csv(csv)
+    iters = data['iter'].tolist()
+    if 'TRD' in d:
+        dir_path = os.path.join(HOME, 'log_dir', d)
+        print(dir_path)
+        shutil.rmtree(dir_path)
+        print('removed')
 
-        if iters[-1] != 1464 and not('NSTD-1.0' in d or 'NSTD-0.5' in d) and not('TRD' in d):
-            print('restarting experiments:', d, iters[-1])
-            model_spec = os.path.join(HOME, 'model_specs', '{}.json'.format(d))
-            subcommand = "python3 run.py --server_type LEONHARD --model_spec {} --restore_iter {}".format(model_spec, iters[-1])
-            command = "bsub -n 8 '{}'".format(subcommand)
-            os.system(command)
-    except:
-        pass
+    if iters[-1] != 1464 and not('NSTD-1.0' in d or 'NSTD-0.5' in d) and not('TRD' in d):
+        print('restarting experiments:', d, iters[-1])
+        model_spec = os.path.join(HOME, 'model_specs', '{}.json'.format(d))
+        subcommand = "python3 run.py --server_type LEONHARD --model_spec {} --restore_iter {}".format(model_spec, iters[-1])
+        command = "bsub -n 8 '{}'".format(subcommand)
+        os.system(command)
