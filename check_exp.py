3bab2d833b746a10198a6a749c637a3815a7f073
sahanayvaz <sahanayvaz@gmail.com>
2019-12-08 13:35:21 +0100

    changed check_exp

--- check_exp.py
+++ check_exp.py
@@ -19,16 +19,17 @@ for d in dirs:
 dirs = os.listdir(dir_path)
 for d in dirs:
     try:
-        csv = os.path.join(dir_path, d, 'inter', 'progress.csv')
-        data = pd.read_csv(csv)
-        iters = data['iter'].tolist()
+        if 'MARIO' in d:
+            csv = os.path.join(dir_path, d, 'inter', 'progress.csv')
+            data = pd.read_csv(csv)
+            iters = data['iter'].tolist()
 
-        if iters[-1] != 1464 and not('NSTD-1.0' in d or 'NSTD-0.5' in d):
-            print('restarting experiments:', d, iters[-1])
-            model_spec = os.path.join(HOME, 'model_specs', '{}.json'.format(d))
-            subcommand = "python3 run.py --server_type LEONHARD --model_spec {} --restore_iter {}".format(model_spec, iters[-1])
-            command = "bsub -n 8 '{}'".format(subcommand)
-            os.system(command)
+            if iters[-1] != 1464 and not('NSTD-1.0' in d or 'NSTD-0.5' in d):
+                print('restarting experiments:', d, iters[-1])
+                model_spec = os.path.join(HOME, 'model_specs', '{}.json'.format(d))
+                subcommand = "python3 run.py --server_type LEONHARD --model_spec {} --restore_iter {}".format(model_spec, iters[-1])
+                command = "bsub -n 8 '{}'".format(subcommand)
+                os.system(command)
 
     except:
         if 'MARIO' in d:
