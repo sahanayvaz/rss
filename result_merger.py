7ad61a0801712fad5221fd34df62b6b37e8dda18
sahanayvaz <sahanayvaz@gmail.com>
2019-12-09 02:12:37 +0100

    added result_merger

--- result_merger.py
+++ result_merger.py
@@ -5,5 +5,6 @@ log_dir = './log_dir'
 folders = os.listdir(log_dir)
 for f in folders:
     if 'COINRUN' in f:
-        inters = os.listdir(f)
+        path = os.path.join(log_dir, f)
+        inters = os.listdir(path)
         print(inters)
