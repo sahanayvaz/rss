4688e8fe6c94fabfcc900434b01194a00beac360
sahanayvaz <sahanayvaz@gmail.com>
2019-12-09 02:24:36 +0100

    added result_merger

--- result_merger.py
+++ result_merger.py
@@ -13,18 +13,15 @@ for f in folders:
                 df = [None, None]
                 for i in inters:
                     if 'inter-' in i:
-                        try:
-                            i_path = os.path.join(path, i, 'progress.csv')
-                            df2 = pd.read_csv(i_path)
-                            df[1] = df2
-                        except:
-                            print(i_path)
-                            sys.exit()
+                        i_path = os.path.join(path, i, 'progress.csv')
+                        df2 = pd.read_csv(i_path)
+                        df[1] = df2
                     elif 'inter' in i:
                         i_path = os.path.join(path, i, 'progress.csv')
                         df1 = pd.read_csv(i_path)
                         df[0] = df1
                 df = pd.concat(df)
-                df.to_csv(os.path.join(path, 'inter', 'test.csv'))
+                df.to_csv(os.path.join(path, 'inter', 'progress.csv'), index=False)
             except:
+                print(f)
                 pass
